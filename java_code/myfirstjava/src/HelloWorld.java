6e7bae44d6092d960577325a20a9335d7de5f718
jerrychane <jerrychane@126.com>
2021-03-22 22:43:59 +0800

    lear java the 2 days

--- java_code/myfirstjava/src/HelloWorld.java
+++ java_code/myfirstjava/src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !");
+        String hobby = "I love learn";
+        System.out.println(hobby);
     }
 }
-
