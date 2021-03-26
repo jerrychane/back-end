da6e4d4c4a07787352ccaded1712f935321e01c8
jerrychane <jerrychane@126.com>
2021-03-26 22:15:34 +0800

    learn condition sentence

--- java_code/myfirstjava/src/HelloWorld.java
+++ java_code/myfirstjava/src/HelloWorld.java
@@ -1,17 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World !");
-        String hobby = "I love learn";
-        System.out.println(hobby);
+//        System.out.println("Hello World !");
+//        String hobby = "I love learn";
+//        System.out.println(hobby);
+        int age = 25;
+        if(age>60){
+            System.out.println("老年");
+        } else if(age>40){
+            System.out.println("中年");
+        } else if(age>18) {
+            System.out.println("青年");
+        } else {
+            System.out.println("童年");
+        }
     }
-}
-public class Dog {
-    String breed;
-    int size;
-    String color;
-    int age;
-    void eat() { }
-    void run() { }
-    void sleep() { }
-    void name() { }
 }
